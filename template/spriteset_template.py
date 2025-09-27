8b32c66141e33b91a1d1403c88e2a9af138d5895
Su1phur <icooltse@gmail.com>
2025-09-27 23:00:59 +0100

    BR710 Added.

--- template/spriteset_template.py
+++ template/spriteset_template.py
@@ -4,13 +4,13 @@ spriteset(spriteset_{unit}, "{path}\\{unit}_1x_8bpp.png")
     {{ template_XXtrains_1x(0,0) }}
 
     alternative_sprites(spriteset_{unit}, ZOOM_LEVEL_NORMAL, BIT_DEPTH_32BPP, "{path}\\{unit}_1x_32bpp.png", "{path}\\{unit}_1x_mask.png")
-        {{ template_XXtrains_1x(0,0) }}
+    {{ template_XXtrains_1x(0,0) }}
 
     alternative_sprites(spriteset_{unit}, ZOOM_LEVEL_IN_2X, BIT_DEPTH_32BPP, "{path}\\{unit}_2x_32bpp.png", "{path}\\{unit}_2x_mask.png")
-        {{ template_XXtrains_2x(0,0) }}
+    {{ template_XXtrains_2x(0,0) }}
 
     alternative_sprites(spriteset_{unit}, ZOOM_LEVEL_IN_4X, BIT_DEPTH_32BPP, "{path}\\{unit}_4x_32bpp.png", "{path}\\{unit}_4x_mask.png")
-        {{ template_XXtrains_4x(0,0) }}
+    {{ template_XXtrains_4x(0,0) }}
 
 // purchase sprite
 spriteset(spriteset_{unit}_purchase, "{path}\\{unit}_1x_8bpp.png")
